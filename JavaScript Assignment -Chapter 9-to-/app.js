24abc09d84d28e1f6ce7a7bfdcc46dceda972f12
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-01 01:27:38 -0800

    if else question 2 solved

---
+++ JavaScript Assignment -Chapter 9-to-/app.js
@@ -0,0 +1,25 @@
+
+// -------------------Question 1------------------------------
+
+
+// var cityName =prompt("Enter your city name?");
+// if(cityName.toLowerCase() === "karachi")
+// {
+//     console.log("Welcome to Karachi the city of light")
+// }
+
+// -------------------Question 2------------------------------
+
+// var gender = prompt("Please enter your Gender");
+// if(gender.toLowerCase() === "male")
+// {
+//     console.log("Good Morning Sir")
+// }
+// else if( gender.toLowerCase() === "female")
+// {
+//    console.log("Good Morning Ma'am.")
+// }
+// else 
+// {
+//     console.log("enter a valid gender")
+// }
