f0ce0b9fa89481dc0571cc7d35d28bc3f7c9e732
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-02 23:33:40 -0800

    Q7 completed

--- JavaScript Assignment-Chapter 14-to-16/app.js
+++ JavaScript Assignment-Chapter 14-to-16/app.js
@@ -25,3 +25,17 @@ var have_Id_card = [true , false]
 var mix_array = ["Hello",18 ,true , "shahzaib"]
 
 // ---------------------QUESTION (7)---------------------------------------
+
+var qualification = ["SSC","HSC" , "BCS" , "BS" , "BCOM" ,"MS" ,"M" ,"PHIL" ,"PHD"]
+
+// console.log("Qualification")
+// console.log("1 )" + qualification[0]);
+// console.log("2 )" + qualification[1]);
+// console.log("3 )" + qualification[2]);
+// console.log("4 )" + qualification[3]);
+// console.log("5 )" + qualification[4]);
+// console.log("6 )" + qualification[5]);
+// console.log("7 )" + qualification[6]);
+// console.log("8 )" + qualification[7]);
+
+// ---------------------QUESTION (8)---------------------------------------
