c9a175d5d542700abd60954a622acf3b37d570cd
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-10 11:11:41 -0800

    completed Q1

--- Code_Tester_Ground/app.js
+++ Code_Tester_Ground/app.js
@@ -1,28 +1,42 @@
-// var str = "I like color blue because it is the sign of peace";
-// var str= prompt("write any sentence using blue color")
-// var color_found =false;
-// var str_color = str.indexOf("blue");
-// console.log(str_color)
 
-// if(str_color !== -1)
-// {
-//     var before_text =  str.slice(0,str_color);
-// console.log(before_text);
-// var after_text = str.slice(str_color+4);
-// console.log(after_text);
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var My_date = new Date();
+// console.log(My_date);
+// var My_day = My_date.getDay();
+// console.log(dayNames[My_day]);
 
-// var final_text = before_text + " green " + after_text + ".";
-// console.log(final_text)
-// color_found =true;
-// }
-// if(color_found === false)
-// {
-//     console.log(" blur color is not found")
-// }
+// var month = My_date.getMonth();
+// console.log(month+1)
 
-var text =" i like blue and dark blue";
-console.log(text);
+// var year = My_date.getFullYear();
+// console.log(year)
 
-var new_Color = text.replace(/blue/g, "green")
-console.log(new_Color)
+// var minute = My_date.getMinutes();
+// console.log(minute)
 
+
+// var fullTime = My_date.getTime();
+// console.log(fullTime)
+
+// ----------------------------
+
+// var today = new Date();
+// console.log(today);
+// var future = new Date("june 30 2035")
+// console.log(future)
+
+// var ms_today = today.getTime();
+// console.log(ms_today)
+// var ms_future = future.getTime();
+// console.log(ms_future)
+
+// var Diff = ms_future - ms_today
+// console.log(Diff)
+
+// var dDiff = Diff / (1000*60*60*24);
+// console.log(Math.floor(dDiff))
+
+var d = new Date();
+console.log(d);
+var d2 = d.setFullYear(2020);
+console.log(d2);
