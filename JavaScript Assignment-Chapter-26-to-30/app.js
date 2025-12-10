83f54cd81a25713310b741d987871cf2953d1a8f
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-10 01:37:30 -0800

    completed Q2

--- JavaScript Assignment-Chapter-26-to-30/app.js
+++ JavaScript Assignment-Chapter-26-to-30/app.js
@@ -14,3 +14,20 @@
 
 // var ceil_integer = Math.ceil(user_integer)
 // console.log("Ceil value : "+ceil_integer);
+
+// ------------------Qustion (2)--------------------
+
+// var user_integer = prompt("Enter a positive integer \n Example 1.4 0.3 or 4.6 ");
+// var user_integer = -2.673; //for direct value assign
+// console.log("Number : "+user_integer);
+
+// var round_integer = Math.round(user_integer)
+// console.log("Round off value : "+round_integer);
+
+// var floor_integer  = Math.floor(user_integer)
+// console.log("Floor value : "+floor_integer);
+
+// var ceil_integer = Math.ceil(user_integer)
+// console.log("Ceil value : "+ceil_integer);
+
+// ------------------Qustion (3)--------------------
