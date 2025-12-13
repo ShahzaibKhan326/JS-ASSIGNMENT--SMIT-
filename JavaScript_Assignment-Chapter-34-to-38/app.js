ddcc0e3146a97b5eded04954e3cd5f25a9058983
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-13 22:39:15 +0500

    completed Q4

--- JavaScript_Assignment-Chapter-34-to-38/app.js
+++ JavaScript_Assignment-Chapter-34-to-38/app.js
@@ -29,3 +29,37 @@
 // }
 
 // add(5,3);
+
+// -------------Question (4)---------------------------------
+
+// function calulater(num1 , num2 ,operater)
+// {
+//   if(operater === "+")
+//   {
+//     console.log("Sum of " + num1 + " and " + num2 + " = " + (num1+num2))
+//   }
+//   else if(operater === "/")
+//   {
+//     console.log("Divident of " + num1 + " and " + num2 + " = " + (num1/num2))
+//   }
+//   else if(operater === "*")
+//   {
+//     console.log("Muiltiplication of " + num1 + " and " + num2 + " = " + (num1*num2))
+//   } else if(operater === "-")
+//   {
+//     console.log("Subtaction of " + num1 + " and " + num2 + " = " + (num1-num2))
+//   } else if(operater === "%")
+//   {
+//     console.log("Remainder of " + num1 + " and " + num2 + " = " + (num1%num2))
+//   }
+//   else 
+//   {
+//     console.log("Please enter Valid sign")
+//   }
+// }
+
+// var user_num1 = Number(prompt("Enter your First Number."))
+// var user_num2 = Number(prompt("Enter your Second Number."))
+// var user_operater = prompt("Enter your desire operater \n like :(+,-,%,/,*).")
+
+// calulater(user_num1,user_num2,user_operater);
