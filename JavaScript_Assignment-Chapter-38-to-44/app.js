fa42c287fcd7dbd95592789e1019afa91eecfe4f
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-16 22:07:57 -0800

    completed Q2

--- JavaScript_Assignment-Chapter-38-to-44/app.js
+++ JavaScript_Assignment-Chapter-38-to-44/app.js
@@ -10,3 +10,28 @@
 // let num2 = 2;
 // console.log(num1 + " upon " + num2 + " Equals : " +power(num1,num2)  )
 // console.log()
+
+// -----------------Qustion(2)-----------------------------
+
+// ----------A--------------------
+// function leapYear(year)
+// {
+//     if((year % 4 === 0 && year % 100 !== 0)|| (year % 400) === 0)
+//     {
+//         return year + " is a leap year";
+//     }
+//     else
+//     {
+//         return year + " is not a leap year";
+//     }
+//     return (year % 4 === 0 && year % 100 !== 0)|| (year % 400 === 0) ? "leap year" : "not leap year"
+// }
+
+// ----------B--------------------
+// function leapYear(year)
+// {
+//     return (year % 4 === 0 && year % 100 !== 0)|| (year % 400 === 0) ? "leap year" : "not leap year"
+// }
+
+// --------Result----------------
+// console.log(leapYear(2000))
