cfdcb1fd342d91112ea5c836c43f0fe1947212f3
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-16 21:29:17 -0800

    Added Chapter 38-to-44

--- Code_Tester_Ground/Practice_Ground/app.js
+++ Code_Tester_Ground/Practice_Ground/app.js
@@ -1,24 +1,39 @@
-const expr = "Papayas";
-switch (expr) {
-  case "Oranges":
-    console.log("Oranges are $0.59 a pound.");
-    break;
-  case "Mangoes":
-  case "Papayas":
-    console.log("Mangoes and papayas are $2.79 a pound.");
-    // Expected output: "Mangoes and papayas are $2.79 a pound."
-    break;
-  default:
-    console.log(`Sorry, we are out of ${expr}.`);
-}
+// const expr = "Papayas";
+// switch (expr) {
+//   case "Oranges":
+//     console.log("Oranges are $0.59 a pound.");
+//     break;
+//   case "Mangoes":
+//   case "Papayas":
+//     console.log("Mangoes and papayas are $2.79 a pound.");
+//     // Expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${expr}.`);
+// }
 
-let num =4;
+// let num =4;
 
-switch(num)
-{
-case (4):
-    console.log("it 4");
-    break;
-  default :
-  console.log("not 4")
-}
+// switch(num)
+// {
+// case (4):
+//     console.log("it 4");
+//     break;
+//   default :
+//   console.log("not 4")
+// }
+
+// ---------------(while loop)-------------------
+
+// var i = 0;
+// while(i<4)
+// {
+//   console.log("Hello world");
+//   i++;
+// }
+
+// var i = 0;
+// do {
+//   console.log("Hello world!");
+//   i++;
+// } while(i<3)
