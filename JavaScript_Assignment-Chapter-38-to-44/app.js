d086e0b9e031b2ba73b731ca6b97ffd25adaf64a
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-16 21:42:05 -0800

    completed Q1

---
+++ JavaScript_Assignment-Chapter-38-to-44/app.js
@@ -0,0 +1,12 @@
+// -------------------Chapter (38-To-44)---------------------------
+
+// -----------------Qustion(1)-----------------------------
+
+// function power(a,b)
+// {
+//     return Math.pow(a,b)
+// }
+// let num1 = 2;
+// let num2 = 2;
+// console.log(num1 + " upon " + num2 + " Equals : " +power(num1,num2)  )
+// console.log()
