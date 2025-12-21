fd71d1fdc7f013812cad1260f37289f9a406ddf3
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-20 22:18:28 -0800

    completed Q2

--- JavaScript-Assignment-Chapter-49-to-52/app.js
+++ JavaScript-Assignment-Chapter-49-to-52/app.js
@@ -6,5 +6,44 @@ function checkFomData()
     var password = document.getElementById("password").value;
     var display_box = document.getElementById("display-box");
  
+    var validEmail =( email === "shahzaib");
+    var validPassword =( password === "123456")
+
+    if(email === "" || password === "")
+    {
+        display_box.textContent = "Please fill all the feilds"
+        return;
+    }
     
+    if(validEmail && validPassword)
+    {
+        display_box.textContent = "Login Succusfully"
+    }
+    else 
+    {
+        display_box.textContent = "Invalid Email or Password"
+    }
+       
 }
+
+// --------------------Expand Paragraph-------------------------
+
+function expandPara()
+{
+    
+
+     var hiddenPara = document.getElementById("hiddenPara");
+     var toggleButton = document.getElementById("toggleButton")
+     if(hiddenPara.style.display === "none")
+     {
+        hiddenPara.style.display = "inline"
+        toggleButton.textContent ="show less"
+
+     }
+     else 
+     {
+        hiddenPara.style.display = "none"
+        toggleButton.textContent ="show More"
+     }
+}
+
