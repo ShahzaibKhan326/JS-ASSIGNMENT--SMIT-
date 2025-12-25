e7c08d87cd5d19dae98130b2cddedbce353e9211
ShahzaibKhan326 <kshahzaib07@gmail.com>
2025-12-25 10:31:28 -0800

    Added Table Manipulation Practice

---
+++ Code_Tester_Ground/Table_Manipulation/app.js
@@ -0,0 +1,42 @@
+// console.log("Hello world!")
+
+// ---------------------Accessing Table-------------------------------
+
+// ------------(Checking Row in a Table)------------------->
+
+// const table = document.querySelector("#myTable");
+// console.log(table.rows.length);
+
+// ------------(Adding a Row dynamically)------------------->
+
+// function addRow()
+// {
+//     const  table = document.querySelector("#myTable");
+//     const row = table.insertRow();
+
+//     row.insertCell(0).textContent ="shahzaib";
+//     row.insertCell(1).textContent ="20";
+//     row.insertCell(2).textContent ="2354";
+//     row.insertCell(3).textContent =""
+// }
+
+// ------------(Adding 3 Rows using loop)--------------------------------
+
+function addRow()
+{
+       const table = document.querySelector("#myTable");
+        
+  
+     for(let i = 0 ; i < 3 ;i++)
+     {
+      
+      const row = table.insertRow();
+    
+      for(let j = 0 ; j < 4 ; j++)
+      {
+        row.insertCell(j).textContent ="";
+      }
+
+     }
+
+}
